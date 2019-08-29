730071d0b8a65dacc05a697a3d2ef9b3aeb74267
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2019-08-29 16:00:54 +0100

    fetch done

--- 21-fetch/index.js
+++ 21-fetch/index.js
@@ -1,8 +1,9 @@
+// get all the DOM elements you need to work with
+const pokeForm = document.querySelector('#poke-form')
+const pokeList = document.querySelector('#poke-list')
+
 // this function takes a pokemon object and adds it to the list on the page
 function addPokemon (pokemon) {
-  // find the things you need
-  const ul = document.querySelector('#poke-list')
-
   // create & edit things
   const li = document.createElement('li')
   li.className = 'poke-item'
@@ -10,11 +11,39 @@ function addPokemon (pokemon) {
   // create innerHTML
   li.innerHTML = `
     <h3>${pokemon.name}</h3>
+    <button class='delete-btn'>X</button>
     <img src='${pokemon.image}' />
   `
 
+  const deleteBtn = li.querySelector('.delete-btn')
+  deleteBtn.addEventListener('click', event => {
+    deletePokemon(pokemon.id).then(() => li.remove())
+  })
+
   // append to the page
-  ul.append(li)
+  pokeList.append(li)
+}
+
+// listen to the form and create a new pokemon on submission
+function listenToPokeForm () {
+  const listener = event => {
+    event.preventDefault()
+
+    const pokemon = {
+      name: pokeForm.name.value,
+      image: pokeForm.image.value
+    }
+
+    // optimistic rendering
+    // createPokemon(pokemon)
+    // addPokemon(pokemon)
+
+    // conditional rendering
+    createPokemon(pokemon).then(addPokemon)
+  }
+
+  pokeForm.addEventListener('submit', listener)
+  // listener(Event)
 }
 
 // This function takes a Response object and returns parsed JSON from it
@@ -27,9 +56,27 @@ function parseJson (resp) {
   return resp.json()
 }
 
+// creates a pokemon on the server
+function createPokemon (pokemon) {
+  return fetch('http://localhost:3000/pokemons', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(pokemon)
+  }).then(resp => resp.json())
+}
+
+// remove a pokemon from the server
+function deletePokemon (pokemonId) {
+  return fetch(`http://localhost:3000/pokemons/${pokemonId}`, {
+    method: 'DELETE'
+  }).then(resp => resp.json())
+}
+
 // this code returns a Promises(Response), then Promise(Array)
 fetch('http://localhost:3000/pokemons')
   .then(parseJson)
   .then(pokemons => {
     pokemons.forEach(addPokemon)
   })
+
+listenToPokeForm()
